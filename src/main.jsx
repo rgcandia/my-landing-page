beddf8df29c55149a0e73a7afa2f65f6240af033
Ale Candia <alejandrogcandia@gmail.com>
2024-08-13 11:51:45 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <RouterProvider router={router}/>
+    </ThemeProvider>
+    
+  </React.StrictMode>
+);
---
+++ src/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+// Create a theme instance.
+//Cambiar los colores
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
+
+export default theme;
